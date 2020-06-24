566a2ae0e63b1bed108bfe432cdac4c397ec7915
Joona Uutela <joona.uutela@gmail.com>
2020-06-24 03:38:19 +0300

    Configured mongoose connection.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
+const mongoose = require('mongoose');
+require('dotenv').config()
 
+const MONGODB_URI = `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.6n5us.mongodb.net/<dbname>?retryWrites=true&w=majority`
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connection to MongoDB:', error.message)
+    })
 
 const typeDefs = gql`
     type Query {
