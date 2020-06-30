a90bcfc917f88f4507d27263f379918eff7c1ac3
Joona Uutela <joona.uutela@gmail.com>
2020-06-30 20:56:16 +0300

    Added routes and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
+
+import Home from './Views/Home'
+import Login from './Views/Login'
+import Register from './Views/Register'
+import Navbar from './Components/Navbar'
+
+import 'semantic-ui-css/semantic.min.css'
 import './App.css'
 
 const App = () => {
   return (
-    <div><h1>hi</h1></div>
+    <Router>
+      <Container>
+        <Navbar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Container>
+    </Router>
   );
 }
 
