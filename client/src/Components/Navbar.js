1cf837dfc90a6afefcc5dd23c604bbf9e4f45ee5
Joona Uutela <joona.uutela@gmail.com>
2020-07-01 13:19:15 +0300

    Started working on front page UI (rough version)

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     const handleItemClick = (e, { name }) => setActiveItem(name)
 
     return (
-        <Menu pointing secondary size="massive" color="teal">
+        <Menu pointing secondary size="large" color="teal">
             <Menu.Item
                 name='home'
                 active={activeItem === 'home'}
--- client/src/Views/Home.js
+++ client/src/Views/Home.js
@@ -1,11 +1,48 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { Card, Icon } from 'semantic-ui-react'
+import gql from 'graphql-tag'
 
 const Home = () => {
+    const { loading, data } = useQuery(FETCH_POSTS_QUERY)
+
+    if (loading) {
+        return <h1>loading</h1>
+    }
     return (
         <div>
-            Home
-        </div>
+            <h1>All posts</h1>
+
+            <Card fluid color='red'>
+                <Card.Content>
+                    <Card.Header>Cool Reactjs video</Card.Header>
+                    <Card.Meta>Posted 1.7.2020</Card.Meta>
+                    <Card.Description>
+                        Daniel is a comedian living in Nashville.
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <a style={{ "fontSize": "1.2rem" }}>
+                        10 likes
+                    </a>
+                    <Icon name='like' color='green' style={{ "paddingLeft": "1%" }} size="large" />
+                </Card.Content>
+            </Card>
+
+
+            <Card fluid color='orange' header='Option 2' />
+            <Card fluid color='yellow' header='Option 3' />
+
+        </div >
     )
 }
+
+const FETCH_POSTS_QUERY = gql`
+   {
+    getPosts{
+        title
+    }
+  }
+`
 
 export default Home
